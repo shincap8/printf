634a25f2e3acae12318cdcf5ef8a2c308d272947
shincap8 <shincap8@gmail.com>
2019-10-30 03:28:24 +0000

    Correct betty style to las commit

--- print_number.c
+++ print_number.c
@@ -50,3 +50,45 @@ int print_number(int n)
 	}
 	return (cont);
 }
+/**
+* print_uns - prints an unsigned integer
+* @n: integern
+*
+* Description: this function prints an unsigned integer
+* Return: returns the number of digits printed
+*/
+int print_uns(unsigned int n)
+{
+	int m = 1, c = 0, cont = 0;
+	unsigned int p;
+
+	p = n;
+	if (p > 9)
+	{
+		while (p > 9)
+		{
+			c++;
+			m = m * 10;
+			p = p / 10;
+		}
+		p = n;
+		_putchar('0' + (p / m));
+		m = m / 10;
+		cont++;
+		while (c > 0)
+		{
+			p = n;
+			p = p / m;
+			_putchar('0' + (p % 10));
+			cont++;
+			m = m / 10;
+			c--;
+		}
+	}
+	else
+	{
+		_putchar('0' + n);
+		cont++;
+	}
+	return (cont);
+}
